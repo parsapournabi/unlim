4c6a0242846f1ca419a84074cd7b5915e4b5c947
Wearily <parsapournabi1382@gmail.com>
2025-01-16 22:14:59 +0330

    bottleOfWater.py Done

--- Challenges/bottleOfWater.py
+++ Challenges/bottleOfWater.py
@@ -1,19 +1,12 @@
-import time
-import random
-
 n, m = list(map(int, input().split()))
 t = list(map(int, input().split()))
 
-primes = [2, 3, 5, 7]
+dp = [n + 1] * (n + 1)
+dp[0] = 0
 
-
-def set_zero_with_lowest_movements(numbers, n):
-    st = time.time()
-
-
-
-
-numbers = [random.randint(1, 10) for _ in range(10)]
-print(numbers)
-result = set_zero_with_lowest_movements(numbers, n)
-print(result)
+for i in range(1, n + 1):
+    for tt in t:
+        if i - tt < 0:
+            continue
+        dp[i] = min(dp[i], 1 + dp[i - tt])
+print(dp[n] if dp[n] != n + 1 else "failed")
